46a98c7c87fc046c5a4589e69ffddbc019542a7e
zhaofei <zhaofei@lvmama.com>
2018-04-19 20:25:00 +0800

    first commit

---
+++ src/interpreter/Expression.java
@@ -0,0 +1,7 @@
+package interpreter;
+
+import java.util.Map;
+
+public interface Expression {
+	public int interpret(final Map<String, Expression> variables);
+}
---
+++ src/interpreter/Interpreter.java
@@ -0,0 +1,10 @@
+package interpreter;
+
+public class Interpreter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
